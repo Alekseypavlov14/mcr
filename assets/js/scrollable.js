8b5e5156512c6c5fc50764d16137eaa7970e050d
Alekseypavlov14 <aleshapavlov9@gmail.com>
2025-10-16 11:22:49 +0300

    updated scrollables

--- assets/js/scrollable.js
+++ assets/js/scrollable.js
@@ -1,12 +1,15 @@
 // scrollable
 const scrollableBlocks = Array.from(document.querySelectorAll('[data-scrollable]'))
 const scrollingClass = 'scrolling'
+const activeClass = 'active'
 
 const SCROLL_TIMEOUT = 50
 const MIN_SCROLL_DISTANCE = 100
 
 scrollableBlocks.forEach(scrollable => {
+  const slider = scrollable.querySelector('[data-scrollable-slider]')
   const items = Array.from(scrollable.querySelectorAll('.scrollable__item'))
+  const links = Array.from(scrollable.querySelectorAll('[data-scrollable-link]'))
 
   // initial scrollable scrollLeft
   let startScrollX = 0
@@ -21,12 +24,12 @@ scrollableBlocks.forEach(scrollable => {
   scrollToNearest()
 
   // for mouse
-  scrollable.addEventListener('mousedown', (e) => {
+  slider.addEventListener('mousedown', (e) => {
     if (isSnapping) return
 
-    scrollable.classList.add(scrollingClass)
+    slider.classList.add(scrollingClass)
 
-    startScrollX = scrollable.scrollLeft
+    startScrollX = slider.scrollLeft
     startMouseX = e.clientX
 
     isMouseDown = true
@@ -39,26 +42,26 @@ scrollableBlocks.forEach(scrollable => {
     const currentX = e.clientX
     const dx = currentX - startMouseX
     
-    scrollable.scrollTo({ left: startScrollX - dx })
+    slider.scrollTo({ left: startScrollX - dx })
   })
   window.addEventListener('mouseup', (e) => {
     if (isSnapping) return
     
-    scrollable.classList.remove(scrollingClass)
+    slider.classList.remove(scrollingClass)
 
     // scroll till next item if needed
     const itemsCoordinates = getItemsCoordinates()
-    const currentScrollableX = scrollable.scrollLeft
+    const currentScrollableX = slider.scrollLeft
     const scrollDistance = e.clientX - startMouseX
 
     // scroll to target item
     if (Math.abs(scrollDistance) >= MIN_SCROLL_DISTANCE) {
       if (scrollDistance > 0) {
         const targetItemCoordinate = itemsCoordinates.findLast(coordinate => coordinate < currentScrollableX)
-        if (targetItemCoordinate > 0) scrollable.scrollTo({ left: targetItemCoordinate, behavior: 'smooth' })
-      } else if (scrollDistance < 0) {
+        if (targetItemCoordinate > 0) slider.scrollTo({ left: targetItemCoordinate, behavior: 'smooth' })
+      } else if (scrollDistance < 0) {  
         const targetItemCoordinate = itemsCoordinates.find(coordinate => coordinate > currentScrollableX)
-        if (targetItemCoordinate > 0) scrollable.scrollTo({ left: targetItemCoordinate, behavior: 'smooth' })
+        if (targetItemCoordinate > 0) slider.scrollTo({ left: targetItemCoordinate, behavior: 'smooth' })
       }
     }
     // otherwise go back 
@@ -66,47 +69,57 @@ scrollableBlocks.forEach(scrollable => {
       scrollToNearest()
     }
 
-    startScrollX = scrollable.scrollLeft
+    startScrollX = slider.scrollLeft
     isMouseDown = false
   })
   
   // for touch devices
-  scrollable.addEventListener('touchstart', () => {
-    scrollable.classList.add(scrollingClass)
+  slider.addEventListener('touchstart', () => {
+    slider.classList.add(scrollingClass)
     isTouching = true
   })
   window.addEventListener('touchend', () => {
-    scrollable.classList.remove(scrollingClass)
+    slider.classList.remove(scrollingClass)
     isTouching = false
   })
 
   // technical
   window.addEventListener('resize', () => {
     scrollToNearest()
+
+    startScrollX = 0
+    startMouseX = 0
   })
-  scrollable.addEventListener('scrollend', () => {
+  slider.addEventListener('scrollend', () => {
     if (isMouseDown || isTouching) return
 
     scrollToNearest()
+  })
+
+  // link nav
+  links.forEach((link, index) => {
+    link.addEventListener('click', () => {
+      const coordinates = getItemsCoordinates()
+      slider.scrollTo({ left: coordinates[index], behavior: 'smooth' })
+    })
   })
 
   function scrollToNearest() {
     if (isSnapping) return
     isSnapping = true
 
+    // get closest point
     const snaps = getItemsCoordinates()
-    const diffs = snaps.map(snap => Math.abs(snap - scrollable.scrollLeft - snaps[0]))
+    const diffs = snaps.map(snap => Math.abs(snap - slider.scrollLeft))
 
     const minDiff = Math.min(...diffs)
     const minDiffIndex = diffs.indexOf(minDiff)
 
-    const containerSize = parseInt(getComputedStyle(scrollable).getPropertyValue('--container-size'))
-    const containerPadding = parseInt(getComputedStyle(scrollable).getPropertyValue('--container-padding'))
+    // style nav
+    links.forEach(link => link.classList.remove(activeClass))
+    links[minDiffIndex].classList.add(activeClass)
 
-    const scrollableSize = scrollable.clientWidth
-    const scrollMargin = Math.max(0, scrollableSize - containerSize) / 2 + containerPadding
-
-    scrollable.scrollTo({ left: snaps[minDiffIndex] - scrollMargin, behavior: 'smooth' })
+    slider.scrollTo({ left: snaps[minDiffIndex], behavior: 'smooth' })
 
     setTimeout(() => {
       isSnapping = false
@@ -114,8 +127,19 @@ scrollableBlocks.forEach(scrollable => {
   }
 
   function getItemsCoordinates() {
+    const containerSize = parseInt(getComputedStyle(slider).getPropertyValue('--container-size'))
+    const containerPadding = parseInt(getComputedStyle(slider).getPropertyValue('--container-padding'))
+
+    const scrollableSize = slider.clientWidth
+    const scrollMargin = Math.max(0, scrollableSize - containerSize) / 2 + containerPadding
+
     return items.map(item => {
-      return item.getBoundingClientRect().left - scrollable.getBoundingClientRect().left + scrollable.scrollLeft
+      return (
+        item.getBoundingClientRect().left - 
+        slider.getBoundingClientRect().left + 
+        slider.scrollLeft - 
+        scrollMargin
+      )
     })
   }
 })
